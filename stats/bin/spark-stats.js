31a5f9b670c4c777eacb41c8d94d4fa042a050a9
godwill charles <godwillcharles22@gmail.com>
2025-03-10 09:46:49 +0100

    fastify-postgres--implementation

--- stats/bin/spark-stats.js
+++ stats/bin/spark-stats.js
@@ -1,15 +1,49 @@
 import '../lib/instrument.js'
+import Fastify from 'fastify'
+import fastifyPostgres from '@fastify/postgres'
 import { createApp } from '../lib/app.js'
-import { getPgPools } from '@filecoin-station/spark-stats-db'
 
 const {
   PORT = '8080',
   HOST = '127.0.0.1',
   SPARK_API_BASE_URL = 'https://api.filspark.com/',
-  REQUEST_LOGGING = 'true'
+  REQUEST_LOGGING = 'true',
+  DATABASE_URL,
+
 } = process.env
 
 const pgPools = await getPgPools()
+
+const dbFastify = Fastify({ logger: false })
+
+await dbFastify.register(fastifyPostgres, {
+  connectionString: DATABASE_URL,
+  name: 'stats',
+  pool: {
+    min: 0,
+    max: 100,
+    idleTimeoutMillis: 1000,
+    maxLifetimeSeconds: 60
+  }
+})
+
+const pgPools = {
+  stats: dbFastify.pg.stats,
+  evaluate: dbFastify.pg.evaluate,
+  async end() {
+    await dbFastify.close()
+  }
+}
+
+
+export const withDb = async (poolName, queryFn) => {
+  const client = await dbFastify.pg[poolName].connect()
+  try {
+    return await queryFn(client)
+  } finally {
+    client.release()
+  }
+}
 
 const app = await createApp({
   SPARK_API_BASE_URL,
